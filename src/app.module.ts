cf4b14498f6247fd5c9c9bba8817f84edd0b4d6c
Neil Dobson <neil.dobson71@gmail.com>
2021-11-28 21:14:53 +1100

    Add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SettingsModule } from './settings/settings.module';
 import { AccountsModule } from './accounts/accounts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { createConnection } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'settings',
-    autoLoadEntities: true,
-    synchronize: true,
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [databaseConfig],
+  }), 
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    // Use useFactory, useClass, or useExisting
+    // to configure the ConnectionOptions.
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('database.host'),
+      port: +configService.get<number>('database.port'),
+      username: configService.get('database.user'),
+      password: configService.get('database.password'),
+      database: configService.get('database.name'),
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    // connectionFactory receives the configured ConnectionOptions
+    // and returns a Promise<Connection>.
+    connectionFactory: async (options) => {
+      const connection = await createConnection(options);
+      return connection;
+    },
   }), SettingsModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
