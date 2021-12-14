c311d32a1c5d77f3724f2bab0072a7673422da2a
Neil Dobson <neil.dobson71@gmail.com>
2021-12-15 06:03:51 +1100

    Clean-up

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
+  host: process.env.NODE_ENV ? process.env.DATABASE_HOST : 'localhost',
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
