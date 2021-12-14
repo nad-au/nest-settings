7eeb3c071ed5ba6498c9f38e87f85cab3924ce67
Neil Dobson <neil.dobson71@gmail.com>
2021-12-15 06:51:33 +1100

    Switch to Vite+Vue3+Tailwind

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,15 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-const path = require("path");
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': resolve(__dirname, 'src'),
     },
+  },
+  server: {
+    open: true
   },
 })
