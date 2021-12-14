7b7d6232d7b8aa26a261befc44a4c012810f0dfd
Neil Dobson <neil.dobson71@gmail.com>
2021-12-14 21:07:15 +1100

    Run migration on start-up

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,13 +1,17 @@
 module.exports = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'settings',
-    entities: ['dist/**/*.entity.js'],
-    migrations: ['dist/migrations/*.js'],
-    cli: {
-      migrationsDir: 'src/migrations',
-    },
-  };
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+  ssl:
+  process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
+};
